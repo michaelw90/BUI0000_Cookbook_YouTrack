dbd9ed29d5c7dc236cdacfc77f03d707920eb748
Fred Thompson <fred.thompson@buildempire.co.uk>
2015-12-15 09:07:06 +0000

    Increased the YouTrack version to 6.5.16953.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,9 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
-default['cookbook_youtrack']['youtrack']['version'] = '6.5.16713'
+default['cookbook_youtrack']['youtrack']['version'] = '6.5.16953'
 default['cookbook_youtrack']['youtrack']['install_root_dir'] = '/opt/youtrack'
 default['cookbook_youtrack']['youtrack']['data_dir'] = '/root/.YoutrackData'
 default['cookbook_youtrack']['youtrack']['backup_dir'] = '/root/.YoutrackBackup'
-default['cookbook_youtrack']['youtrack']['download_url'] = 'http://download.jetbrains.com/charisma/youtrack-6.5.16713.zip'
+default['cookbook_youtrack']['youtrack']['download_url'] = 'https://download.jetbrains.com/charisma/youtrack-6.5.16953.zip'
 default['cookbook_youtrack']['youtrack']['memory_options'] = '-Xmx1g'
