e66df6ad615e81195695d28e362c3c9bfe09bd4a
Michael Wright <michaelw90@users.noreply.github.com>
2017-07-24 15:01:33 +0100

    Upgrade to 2017.2.34480

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,9 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
-default['cookbook_youtrack']['youtrack']['version'] = '2017.2.32582'
+default['cookbook_youtrack']['youtrack']['version'] = '2017.2.34480'
 default['cookbook_youtrack']['youtrack']['install_root_dir'] = '/opt/youtrack'
 default['cookbook_youtrack']['youtrack']['data_dir'] = '/root/.YoutrackData'
 default['cookbook_youtrack']['youtrack']['backup_dir'] = '/root/.YoutrackBackup'
-default['cookbook_youtrack']['youtrack']['download_url'] = 'https://download.jetbrains.com/charisma/youtrack-2017.2.32582.zip'
+default['cookbook_youtrack']['youtrack']['download_url'] = 'https://download.jetbrains.com/charisma/youtrack-2017.2.34480.zip'
 default['cookbook_youtrack']['youtrack']['memory_options'] = '-Xmx1g'
