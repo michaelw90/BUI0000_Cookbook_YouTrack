7be71a048d2247ddcd1317dbab8fd8ab1f68b7f9
Michael Wright <michael@wserver.co.uk>
2019-01-26 20:28:45 +0000

    Update to 2018.4.48733

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,9 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
-default['cookbook_youtrack']['youtrack']['version'] = '2018.3.47078'
+default['cookbook_youtrack']['youtrack']['version'] = '2018.4.48733'
 default['cookbook_youtrack']['youtrack']['install_root_dir'] = '/opt/youtrack'
 default['cookbook_youtrack']['youtrack']['data_dir'] = '/root/.YoutrackData'
 default['cookbook_youtrack']['youtrack']['backup_dir'] = '/root/.YoutrackBackup'
-default['cookbook_youtrack']['youtrack']['download_url'] = 'https://download-cf.jetbrains.com/charisma/youtrack-2018.3.47078.zip'
+default['cookbook_youtrack']['youtrack']['download_url'] = 'https://download-cf.jetbrains.com/charisma/youtrack-2018.4.48733.zip'
 default['cookbook_youtrack']['youtrack']['memory_options'] = '-Xmx1g'
